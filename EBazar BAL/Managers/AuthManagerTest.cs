1b53d868ac9843c31ef079f9f4680c486e73f332
Rinu Alexandru <alexandru.rinu@s.unibuc.ro>
2023-12-06 22:05:34 +0200

    Added first test

---
+++ EBazar BAL/Managers/AuthManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBazar_BAL.Models;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace EBazar_BAL.Managers
+{
+
+
+    [TestFixture]
+    public class AuthManagerTests
+    {
+        private readonly AuthManager _authManager;
+        private readonly UserManager _userManager;
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task Login_ValidCredentials_ReturnsSuccessResult()
+        {
+            var user = new RegisterModel()
+            {
+                UserName = "username",
+                Email = "testemail@gmail.com",
+                Password = "Password01!",
+                FirstName = "test",
+                LastName = "test",
+                Age = 12,
+                Role = "User"
+            };
+            var key = "dfjfdids";
+            var registerResult = await _authManager.Register(user, key);
+            var readUser = await _userManager.getUserByUsername(user.UserName);
+            Assert.That(registerResult==1, "Failed");
+            Assert.That(readUser!=null, "Failed");
+            }
+        }
+
+    
+}
---
+++ EBazarTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EBazarTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
